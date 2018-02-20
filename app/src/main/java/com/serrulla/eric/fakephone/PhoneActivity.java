4ffc7b336224ce2f9b7e1591b74c09b2595ceee9
eric <eric@serrulla.com>
2018-02-20 21:03:38 +0100

    Number 1 button works

--- app/src/main/java/com/serrulla/eric/fakephone/PhoneActivity.java
+++ app/src/main/java/com/serrulla/eric/fakephone/PhoneActivity.java
@@ -2,13 +2,36 @@ package com.serrulla.eric.fakephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PhoneActivity extends AppCompatActivity {
+
+
+    //private final int btn_id = R.id.btn1;
+    private Button btn_num;
+    private EditText field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
+
+        btn_num = findViewById(R.id.btn1);
+
+        field = findViewById(R.id.phoneNumber);
+        btn_num.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                field.append(btn_num.getText());
+            }
+        });
+
     }
+
+
+
+
 
 }
